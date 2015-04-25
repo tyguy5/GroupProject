67276aa6d4158660dd16afcdedca2603b589076b
daltonpurnell <daltonpurnell@live.com>
2015-04-25 16:22:38 -0600

    adds MFMailComposeViewController
    Feedback buttons on Options screen is linked up and presents an MFMailComposeViewController with my email address pre-populated

---
+++ GroupProject/GroupProject/OptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  OptionsViewController.h
+//  GroupProject
+//
+//  Created by Dalton on 4/25/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionsViewController : UIViewController
+
+@end
