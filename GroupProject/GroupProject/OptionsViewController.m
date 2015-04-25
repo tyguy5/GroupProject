7357053ec48d4f7e07603f122d56033795cf4453
daltonpurnell <daltonpurnell@live.com>
2015-04-25 16:27:47 -0600

    adds MFMailComposeViewController to Options view
    i forgot to include this file in my last commit

--- GroupProject/GroupProject/OptionsViewController.m
+++ GroupProject/GroupProject/OptionsViewController.m
@@ -8,7 +8,10 @@
 
 #import "OptionsViewController.h"
 
-@interface OptionsViewController ()
+@import MessageUI;
+
+
+@interface OptionsViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -18,20 +21,22 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)feedbackButtonTapped:(id)sender {
+    
+    MFMailComposeViewController *mailViewController = [MFMailComposeViewController new];
+    mailViewController.mailComposeDelegate = self;
+    
+    // set up email address to dalton.purnell61@gmail.com so that it is pre-populated
+    [mailViewController setToRecipients:@[@"dalton.purnell61@gmail.com"]];
+    
+    [self presentViewController:mailViewController animated:YES completion:nil];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
-*/
+
 
 @end
