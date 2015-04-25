20085d00d9b47388afbb7b2bc82ebfc13c807a7c
Alan Barth <alancbarth@gmail.com>
2015-04-25 13:14:52 -0600

    Adds Core Data
    Compliments of Dalton and Alan.

---
+++ GroupProject/GroupProject/Entry.m
@@ -0,0 +1,18 @@
+//
+//  Entry.m
+//  GroupProject
+//
+//  Created by Alan Barth on 4/25/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import "Entry.h"
+
+
+@implementation Entry
+
+@dynamic timestamp;
+@dynamic recentLocations;
+@dynamic titleOfEntry;
+
+@end
---
+++ GroupProject/GroupProject/EntryController.m
@@ -0,0 +1,61 @@
+//
+//  EntryController.m
+//  GroupProject
+//
+//  Created by Alan Barth on 4/25/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import "EntryController.h"
+#import "Stack.h"
+
+@implementation EntryController
+
++ (EntryController *) sharedInstance {
+    static EntryController *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [EntryController new];
+    });
+    return sharedInstance;
+}
+
+#pragma mark - Create
+
+- (Entry *) createEntryWithTitle: (NSString *) title {
+    Entry *entry = [NSEntityDescription insertNewObjectForEntityForName:@"Entry" inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    entry.titleOfEntry = title;
+    entry.timestamp = [NSDate date];
+    
+    [self saveToPersistentStorage];
+    
+    return entry;
+    
+}
+
+#pragma mark - Read
+
+//Custom Getting method
+- (NSArray *) entries {
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Entry"];
+    NSArray *fetchObjects = [[Stack sharedInstance].managedObjectContext executeFetchRequest:fetchRequest error:nil];
+    return fetchObjects;
+}
+
+- (void) saveToPersistentStorage {
+    [[Stack sharedInstance].managedObjectContext save:nil];
+}
+
+#pragma mark - Update
+
+-(void) save {
+    [self saveToPersistentStorage];
+}
+
+#pragma mark - Delete
+
+-(void) deleteEntry:(Entry *) entry {
+    [entry.managedObjectContext deleteObject:entry];
+}
+
+@end
