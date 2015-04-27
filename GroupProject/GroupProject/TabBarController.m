28a03d9adc94887df293ad58f6d2aaf28a9c0cc9
daltonpurnell <daltonpurnell@live.com>
2015-04-26 22:18:07 -0600

    changes list tab to default tab instead of add tab

---
+++ GroupProject/GroupProject/TabBarController.m
@@ -0,0 +1,24 @@
+//
+//  TabBarController.m
+//  GroupProject
+//
+//  Created by Dalton on 4/26/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+@interface TabBarController ()
+
+@end
+
+@implementation TabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+@end
