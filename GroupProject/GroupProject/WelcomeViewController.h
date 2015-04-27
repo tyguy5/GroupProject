90128b79abbbb8a19c7b17c22cb661c3833d5b95
daltonpurnell <daltonpurnell@live.com>
2015-04-26 23:18:59 -0600

    changes onboarding process views
    the welcome view is super guetto. but at least it works. we can make it look better later

---
+++ GroupProject/GroupProject/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  GroupProject
+//
+//  Created by Dalton on 4/26/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
