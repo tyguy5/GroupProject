9f26d5aa81c111c54e45637dbb25365e51fc6c31
daltonpurnell <daltonpurnell@live.com>
2015-04-28 11:40:40 -0600

    adds appearance controller
    everything is set to brown until we figure out our color scheme

---
+++ GroupProject/GroupProject/AppearanceController.h
@@ -0,0 +1,15 @@
+//
+//  AppearanceController.h
+//  GroupProject
+//
+//  Created by Dalton on 4/28/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppearanceController : NSObject
+
++ (void)initializeAppearanceDefaults;
+
+@end
---
+++ GroupProject/GroupProject/AppearanceController.m
@@ -0,0 +1,33 @@
+//
+//  AppearanceController.m
+//  GroupProject
+//
+//  Created by Dalton on 4/28/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import "AppearanceController.h"
+#import "TabBarController.h"
+
+#import "LoginViewController.h"
+#import "WelcomeViewController.h"
+
+#import "OptionsViewController.h"
+#import "AddViewController.h"
+
+@import UIKit;
+
+@implementation AppearanceController
+
+
++ (void)initializeAppearanceDefaults {
+    
+    [[UINavigationBar appearance] setBackgroundColor:[UIColor brownColor]];
+    
+    [[UITabBar appearance] setBackgroundColor:[UIColor brownColor]];
+    
+    [[UITabBar appearance] setTintColor:[UIColor brownColor]];
+}
+
+
+@end
