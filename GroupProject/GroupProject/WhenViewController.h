9fe5b1ea7cf1c4e16891ab5005663b358f78da68
daltonpurnell <daltonpurnell@live.com>
2015-04-28 21:36:46 -0600

    adds whenViewController class
    forgot to start new feature first

---
+++ GroupProject/GroupProject/WhenViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhenViewController.h
+//  GroupProject
+//
+//  Created by Dalton on 4/28/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhenViewController : UIViewController
+
+@end
---
+++ GroupProject/GroupProject/WhenViewController.m
@@ -0,0 +1,29 @@
+//
+//  WhenViewController.m
+//  GroupProject
+//
+//  Created by Dalton on 4/28/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import "WhenViewController.h"
+
+@interface WhenViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UITextField *dateSelctionText;
+
+@end
+
+@implementation WhenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    datePicker.datePickerMode = [
+    
+}
+
+
+
+@end
