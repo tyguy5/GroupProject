0ca8ccd6dc782a51b4e97eeefd5f51c7e17c770d
daltonpurnell <daltonpurnell@live.com>
2015-04-28 22:49:20 -0600

    adds AboutViewController class

---
+++ GroupProject/GroupProject/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  GroupProject
+//
+//  Created by Dalton on 4/28/15.
+//  Copyright (c) 2015 Front Row Joes Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
