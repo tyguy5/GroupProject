8d42b2cba6895e53d2a49b47a30f2b80487ccb7a
daltonpurnell <daltonpurnell@live.com>
2015-05-02 16:21:18 -0600

    updates constraints

--- GroupProject/GroupProject/WhenViewController.m
+++ GroupProject/GroupProject/WhenViewController.m
@@ -53,7 +53,6 @@
     self.datePicker2.hidden = NO;
     self.datePicker2.timeZone = [NSTimeZone localTimeZone];
     
-    
     self.datePicker2.datePickerMode = UIDatePickerModeDateAndTime;
     
 }
