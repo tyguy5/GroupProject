de29fdf60d4bd7e22a51c12dcadc539138197c4f
daltonpurnell <daltonpurnell@live.com>
2015-05-08 19:59:36 -0600

    random little changes. they don't matter.
    i just don't want to mess things up by not committing this before i finish this feature.

--- GroupProject/WhereViewController.m
+++ GroupProject/WhereViewController.m
@@ -226,25 +226,9 @@
 
 
 - (IBAction)distanceSlide:(UISlider *)sender {
-    self.radiusLabel.text = [NSString stringWithFormat:@"Radius: %i miles", (int)sender.value];
-//    
-//        if(sender.value<200)
-//            [sender setValue:200];
-//        else{
-//            if ((int)sender.value % 100>50){
-//    
-//                [sender setValue:sender.value+(100-(int)sender.value % 100) ];
-//    
-//            }
-//            else{
-//    
-//                [sender setValue:sender.value-((int)sender.value % 100) ];
-//    
-//            }
-//    
-            [self setNewRadius];
-//
-//        }
+    self.radiusLabel.text = [NSString stringWithFormat:@"Radius: %d miles", (int)sender.value];
+
+    [self setNewRadius];
     
 }
 
